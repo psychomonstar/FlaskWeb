b73fa73d996a8ed138f455e1c730dfb70a3fe48e
zilong <psychomonstar@163.com>
2017-12-26 14:27:48 +0800

    实现发送确认邮件进行注册账户的确认功能

--- FlaskBlog/app/auth/views.py
+++ FlaskBlog/app/auth/views.py
@@ -1,10 +1,11 @@
 # -*-coding:utf-8-*-
 from app import db
+from app.email import send_email
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from .forms import LoginForm, RegistrationForm
 from ..models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -31,9 +32,24 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.mail.data, username=form.username.data,
+        user = User(email=form.email.data, username=form.username.data,
                     password=form.password.data)
         db.session.add(user)
-        flash("注册成功")
-        return redirect(url_for('auth.login'))
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email,'邮件确认','auth/email/confirm',user = user,token=token)
+        flash("已经发送确认邮件")
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
--- FlaskBlog/config.py
+++ FlaskBlog/config.py
@@ -7,15 +7,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '123456'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
+
     MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
+    FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
 
     @staticmethod
     def init_app(app):
