bf072d100a57d8a9577b15a5900f2cd1610f9144
zilong <psychomonstar@163.com>
2017-12-29 16:35:36 +0800

    博文编辑页面

--- FlaskBlog/app/main/views.py
+++ FlaskBlog/app/main/views.py
@@ -15,19 +15,18 @@ from ..models import User, Role, Permission, Post
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-
     form = PostForm()
     if current_user.can(Permission.WRITE_ARTICLES) and form.validate_on_submit():
         post = Post(body=form.body.data, author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    page = request.args.get('page',1,type=int)
-    pagination=Post.query.order_by(Post.timestamp.desc()).paginate(
-        page,per_page=current_app.config['POSTS_PER_PAGE'],
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['POSTS_PER_PAGE'],
         error_out=False
     )
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts,pagination=pagination)
+    return render_template('index.html', form=form, posts=posts, pagination=pagination)
 
 
 @main.route('/user/<username>')
@@ -40,8 +39,7 @@ def user(username):
         page, per_page=current_app.config['POSTS_PER_PAGE'],
         error_out=False)
     posts = pagination.items
-    return render_template('user.html', user=user, posts = posts,pagination=pagination)
-
+    return render_template('user.html', user=user, posts=posts, pagination=pagination)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
@@ -86,3 +84,25 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
+
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and not current_user.can(Permission.ADMINISTER):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('博文已更新')
+        return redirect(url_for('post', id=post.id))
+    form.body.data = post.body
+    return render_template("edit_post.html", form=form)
